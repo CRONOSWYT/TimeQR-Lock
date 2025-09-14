63d6532166822c46cbc8c8474a7dd70ab89cc29a
CRONOSWYT <151398848+CRONOSWYT@users.noreply.github.com>
2025-09-14 11:07:23 -0500

    Update generador qr.js

--- js/generador qr.js
+++ js/generador qr.js
@@ -1,37 +1,30 @@
- function generarQR() {
-        const nombre = document.getElementById("nombre").value.trim();
-        const grado = document.getElementById("grado").value.trim();
-        const documento = document.getElementById("documento").value.trim();
+   function generarQR() {
+  const nombre = document.getElementById("nombre").value.trim();
+  const grado = document.getElementById("grado").value.trim();
+  const documento = document.getElementById("documento").value.trim();
 
-        if (!nombre || !grado || !documento) {
-          alert("Por favor completa todos los campos.");
-          return;
-        }
+  if (!nombre || !grado || !documento) {
+    alert("Por favor completa todos los campos.");
+    return;
+  }
 
-        const datos = JSON.stringify({
-          nombre: nombre,
-          grado: grado,
-          documento: documento,
-        });
+  const datos = JSON.stringify({
+    nombre: nombre,
+    grado: grado,
+    documento: documento,
+  });
 
-        // Encriptar los datos (puedes cambiar la clave secreta)
-        const claveSecreta = "timeqrlock";
-        const datosCifrados = CryptoJS.AES.encrypt(
-          datos,
-          claveSecreta
-        ).toString();
+  // Encriptar los datos (puedes cambiar la clave secreta)
+  const claveSecreta = "timeqrlock";
+  const datosCifrados = CryptoJS.AES.encrypt(datos, claveSecreta).toString();
 
-        // Limpiar QR anterior
-        document.getElementById("qrcode").innerHTML = "";
+  // Limpiar QR anterior
+  document.getElementById("qrcode").innerHTML = "";
 
-        // Generar QR
-        new QRCode(document.getElementById("qrcode"), {
-          text: datosCifrados,
-          width: 256,
-          height: 256,
-        });
-      }
-       window.addEventListener('load', () => {
-      document.getElementById('loader').classList.add('hidden');
-
-    });
+  // Generar QR
+  new QRCode(document.getElementById("qrcode"), {
+    text: datosCifrados,
+    width: 256,
+    height: 256,
+  });
+}
