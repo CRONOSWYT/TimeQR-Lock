a8494838f12367560cbbb2732abde976abe89c3b
CRONOSWYT <151398848+CRONOSWYT@users.noreply.github.com>
2025-11-10 08:30:12 -0500

    Delete js directory

--- js/inicio.js
+++
@@ -1,9 +0,0 @@
-         // Verificamos si hay sesión
-    const logueado = localStorage.getItem("logueado");
-
-    if (logueado !== "true") {
-      // Si no hay sesión, redirige a inicio
-      window.location.href = "https://cronoswyt.github.io/TimeQR-Lock/";
-    } else {
-      // Mostrar
-    }
